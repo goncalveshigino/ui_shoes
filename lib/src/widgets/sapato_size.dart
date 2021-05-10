0dc8e1dc1bfc4e364cc433997b8c294e29ac5110
goncalveshigino <goncalveshigino20@gmail.com>
2021-05-10 16:36:43 +0100

    feito

--- lib/src/widgets/sapato_size.dart
+++ lib/src/widgets/sapato_size.dart
@@ -66,7 +66,7 @@ class _CadaTamanho extends StatelessWidget {
       child: Text(
         '${numero.toString().replaceAll('.0','')}',
         style: TextStyle(
-          color: Color(0xffF1A23A), 
+           color: (this.numero == 9) ?  Colors.white : Color(0xffF1A23A),
           fontWeight: FontWeight.bold, 
           fontSize: 16
         ),
@@ -74,10 +74,15 @@ class _CadaTamanho extends StatelessWidget {
       width: 45, 
       height: 45,
       decoration: BoxDecoration(
-         color: Colors.white, 
+         color: (this.numero == 9) ? Color(0xffF1A23A) : Colors.white, 
          borderRadius: BorderRadius.circular(10), 
          boxShadow: [
-           //TODO: BoxShadow()
+           if(this.numero == 9)
+            BoxShadow(
+              color: Color(0xffF1A23A), 
+              blurRadius: 10, 
+              offset: Offset(0, 5)
+            )
          ]
       ),
     );
@@ -100,7 +105,7 @@ class _SapatoComSombra extends StatelessWidget {
             ),
 
 
-            Image( image: AssetImage('assets/imgs/azul.png'))
+            Image( image: AssetImage('assets/imgs/negro.png'))
          ],
       ),
     );
